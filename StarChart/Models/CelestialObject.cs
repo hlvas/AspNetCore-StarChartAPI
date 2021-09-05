2764249ce72515bd8207ea2926f2b54d7fe91161
Vasilis Iliadis <hlvas@yahoo.com>
2021-09-05 22:08:00 +0300

    Completed module

---
+++ StarChart/Models/CelestialObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id;
+        public int OrbitedObjectId;
+        [Required]
+        public string Name;
+        [NotMapped]
+        List<CelestialObject> Satellites;
+        public TimeSpan OrbitalPeriod;
+    }
+}
