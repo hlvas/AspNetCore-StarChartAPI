86a5d8430f7f7e0abd33e75bde04318ce8059fe3
Vasilis Iliadis <hlvas@yahoo.com>
2021-09-06 22:40:01 +0300

    6 module

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -18,6 +18,51 @@ namespace StarChart.Controllers
             _context = context;
         }
 
+        [HttpPost]
+        public IActionResult Create([FromBody] CelestialObject obj)
+        {
+            _context.CelestialObjects.Add(obj);
+            _context.SaveChanges();
+            return CreatedAtRoute("GetById", new { id = obj.Id }, obj);
+        }
+
+        [HttpPost("{id}")]
+        public IActionResult Update(int id, [FromBody] CelestialObject obj)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+            if (celestialObject == null)
+                return NotFound();
+            celestialObject.Name = obj.Name;
+            celestialObject.OrbitalPeriod = obj.OrbitalPeriod;
+            celestialObject.OrbitedObjectId = obj.OrbitedObjectId;
+            _context.Update(celestialObject);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            var celestialObject = _context.CelestialObjects.Find(id);
+            if (celestialObject == null)
+                return NotFound();
+            celestialObject.Name = name;
+            _context.Update(celestialObject);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(e => e.Id == id);
+            if (!celestialObjects.Any())
+                return NotFound();
+            _context.CelestialObjects.RemoveRange(celestialObjects);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
         [HttpGet("{id:int}", Name = "GetById")]
         public IActionResult GetById(int id)
         {
